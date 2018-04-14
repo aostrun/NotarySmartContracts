3ed61ef4e3cc60668b0fd4598d6d0daf14991f58
Andrijan Ostrun <andrijan.ostrun@gmail.com>
2018-04-14 14:21:13 +0200

    Updated contract

--- contracts/notary_contract.sol
+++ contracts/notary_contract.sol
@@ -4,14 +4,19 @@ contract Notary{
     
     mapping( uint256 => Record ) records;
     
-    uint256 currentId = 0;
+    uint256 currentId;
     
     struct Record{
         uint256 hash;
         // Indicate which address is party on this record
-        mapping( address => bool ) parties;
+        address party1;
+        address party2;
+        
+        bool party1_ack;
+        bool party2_ack;
+        //mapping( address => bool ) parties;
         // Store acknowledgment of this record by the party
-        mapping( address => bool ) allowance;
+        //mapping( address => bool ) allowance;
         // Store the timestamp of record creation
         uint256 createdAt;
         // Record is valid if now < validUntil
@@ -27,7 +32,7 @@ contract Notary{
         Allows access to the record only to party registered to the record
     */
     modifier onlyParty(uint256 recordId){
-        require(records[recordId].parties[msg.sender] == true);
+        //require(records[recordId].parties[msg.sender] == true);
         _;
     }
     
@@ -44,7 +49,7 @@ contract Notary{
         Constructor
     */
     function Notary() public{
-        
+        currentId = 0;
     }
     
     /*
@@ -52,50 +57,75 @@ contract Notary{
     */
     function createRecord(uint256 hash, address[] _parties, uint256 validUntil)
         external
-        returns (uint256)
+        returns (uint256 newRecordId)
     {	
-		
+		newRecordId = currentId++;
 		// Limit max number of parties to 10
-		require(_parties.length <= 10);
+		require(_parties.length == 2);
 		
         // Create new record
-        Record newRecord;
+        Record newRecord; // = Record(0, new address[](0), new address[](0), 0, 0, 0);
         newRecord.hash = hash;
 		newRecord.validUntil = validUntil;
 		
         // Add parties and init values to true
+        /*
         // indicating that address is party on this record
         for(uint8 i = 0; i < _parties.length; i++){
-			newRecord.parties[_parties[i]] = true;
+			newRecord.parties.push(_parties[i]);
 		}
+		*/
+		newRecord.party1 = _parties[0];
+		newRecord.party2 = _parties[1];
 		
         // Save the record
-        records[currentId] = newRecord;
-        uint256 returnId = currentId;
+        records[newRecordId] = newRecord;
+        //uint256 returnId = currentId;
+        emit NewRecordCreated(newRecordId);
         // Increment the pointer
-        currentId++;
-        emit NewRecordCreated(returnId);
-        return (returnId);
+        
     }
     
     function acceptRecord(uint256 recordId)
         external
     {   
-        require(records[recordId].parties[msg.sender] == true);
+        require(validParty(recordId, msg.sender) == true);
         require(records[recordId].validUntil >= now);
         
-        records[recordId].allowance[msg.sender] = true;
+        if(msg.sender == records[recordId].party1){
+            records[recordId].party1_ack = true;
+        }else{
+            records[recordId].party2_ack = true;
+        }
+        //records[recordId].allowances.push(msg.sender);
         
     }
     
+    function validParty(uint256 recordId, address party)
+        constant
+        returns (bool)
+    {   
+        if(party == records[recordId].party1 || party == records[recordId].party2){
+            return true;
+        }
+        return false;
+        /*
+        for(uint8 i = 0; i < records[recordId].parties.length; i++){
+            if(records[recordId].parties[i] == party){
+                return true;
+            }
+        }
+        return false;
+        */
+    }
     
     // Return the record hash 
     function getRecord(uint256 recordId) 
         external 
         constant
-        returns (uint256)
+        returns (uint256, address, uint256)
     {
-        return (records[recordId].hash);
+        return (records[recordId].hash, records[recordId].party1, records[recordId].validUntil);
     }
     
     
